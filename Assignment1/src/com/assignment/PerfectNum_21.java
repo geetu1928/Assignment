65e5fdf955ffe95ea45030356268f5236e08ebc6
geetu <geetu@LAPTOP-MQTDVRCE>
2024-06-26 15:47:27 +0530

    java basic programs

--- Assignment1/src/com/assignment/PerfectNum_21.java
+++ Assignment1/src/com/assignment/PerfectNum_21.java
@@ -1,16 +1,29 @@
 package com.assignment;
 
-public class PerfectNum_21 {
+import java.util.*;
+
+public class PerfectNum_21{
+	public static int perfect(int num) {
+		int res = 0;
+		for (int i = 1; i <= num / 2; i++) {
+			if (num % i == 0)
+				res += i;
+			else
+				continue;
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int n=2;
-		if(n>0) {
-			System.out.println(n + " is Positive");
-		}
-		else {
-			System.out.println(n + " is negative");
-		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value ");
+		int num = sc.nextInt();
+		int res = perfect(num);
+		if (num == res)
+			System.out.println(res + " is a perfect number.");
+		else
+			System.out.println(res + " is not a perfect number.");
+		sc.close();
 
 	}
 
